077105a72f880746102f5fd2f335d04d729915cd
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-08-19 09:13:43 +0300

    Updates

---
+++ precache-manifest.edd1c7327296a559a0ed239d659d0b13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eda2c40eeddb1916754a58bb0eb74bb",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "0429ee3a4c957a598d68",
+    "url": "/todo/static/css/main.3de4cace.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/todo/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "0429ee3a4c957a598d68",
+    "url": "/todo/static/js/main.6726b637.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
